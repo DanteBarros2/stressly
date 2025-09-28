11fab2a84c0589332cb726fef8cb96eb3295cb7b
Jorge Chan <jorgemchan@gmail.com>
2025-09-28 13:11:52 -0300

    segundo commit

--- src/main/kotlin/com/example/stressly/Account.kt
+++
@@ -1,6 +0,0 @@
-package com.example.stressly
-
-data class Account(val id:String? = null
-, val name:String? = null
-, val document:String? = null
-, val state:String? = null    )
--- src/main/kotlin/com/example/stressly/AccountRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.stressly
-
-import org.springframework.data.mongodb.repository.MongoRepository
-import java.util.*
-
-
-interface AccountRepository: MongoRepository<Account, String> {
-    fun findByDocument(document : String) : Optional<Account>
-}
