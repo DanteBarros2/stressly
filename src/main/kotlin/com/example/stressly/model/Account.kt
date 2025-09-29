0c027293ca3f04c82fb6128e980051110cade8fb
Jorge Chan <jorgemchan@gmail.com>
2025-09-28 21:50:32 -0300

    sexto commit

--- src/main/kotlin/com/example/stressly/model/Account.kt
+++ src/main/kotlin/com/example/stressly/model/Account.kt
@@ -1,6 +1,14 @@
 package com.example.stressly.model
 
-data class Account(val id:String? = null
-, val name:String? = null
-, val document:String? = null
-, val state:String? = null    )
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document(collection = "accounts")
+data class Account(
+    @Id
+    val id: String? = null,
+    val username: String,
+    val email: String,
+    var password: String, // será criptografada com BCrypt
+    val role: String = "USER"
+)
